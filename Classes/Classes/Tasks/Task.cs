932ed9551e5a24cdb17697852ae21334e8da4c96
Oksana <my@mail.com>
2018-08-08 00:25:04 +0300

    Create Task class, inherited Bug, Feature, Technical classes. Add enum TaskType. Class TaskBuilder for creating of specific Task.

---
+++ Classes/Classes/Tasks/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Tasks
+{
+    public abstract class Task
+    {
+        public string Name { get; set; }
+        public double Priority { get; set; }
+        public double Sevirity { get; set; }
+        private double dufficulty;
+        public bool IsFixed { get; set; }
+    }
+}
---
+++ Classes/Classes/Tasks/Technical.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Tasks
+{
+    class Technical : Task
+    {
+        public Technical()
+        {
+            Priority = 0.5;
+        }
+    }
+}
