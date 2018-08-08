9a8b3f22759144e113efe75161e0a6762139965b
Anton Petrenko <petrenko.anton22@gmail.com>
2018-08-08 10:19:37 +0300

    {Proto branch}: {initial commit with empty programm skeleton. there are 5 libraries}

---
+++ HelpDesk/HelpDesk/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Feature_Library;
+using Bug_Library;
+using Technical_dept_library;
+using Task_Library;
+using Static_validations;
+
+namespace HelpDesk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //here is our logic
+        }
+    }
+}
---
+++ Technical_dept_library/Technical_dept_library/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technical_dept_library
+{
+    public class Technical_dept
+    {
+        //
+    }
+}
