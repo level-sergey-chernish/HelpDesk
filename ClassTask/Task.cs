38f79a7050f8c49beb20819d385fe2402e735f48
Sergey Chernish <sergey.chernish@gmail.com>
2018-08-11 13:17:05 +0300

    Add new content from RelC

---
+++ ClassTask/Task.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ClassTask
+{
+    public abstract class Task
+    {
+        internal string Name { get; set; }
+        internal double Priority { get; set; }
+        internal double Severity { get; set; }
+        internal double difficulty;
+        internal bool IsFixed = false;
+        private double Difficulty
+        {
+            get
+            {
+                return difficulty;
+            }
+            set
+            {
+                difficulty = TaskDifficulty();
+            }
+        }
+        /// <summary>
+        /// Count difficulty of task. (Priority * Severity)
+        /// </summary>
+        /// <returns> Returns double Difficulty </returns>
+        internal double TaskDifficulty()
+        {
+           return _TaskDifficulty();
+        }
+        private double _TaskDifficulty()
+        {
+            return Difficulty = Priority * Severity;
+        }
+        /// <summary>
+        /// Indicates Task fixed or not, and if it's not fixed - substracts 1 point of difficulty.
+        /// Then check fix again.
+        /// </summary>
+        /// <returns> Returns Boolean meaning IsFixed </returns>
+        public bool Fixed()
+        {
+            if (IsFixed)
+            {
+                Console.WriteLine($"{Name} is fixed!");
+                return IsFixed;
+            }
+            bool tryFix;
+            Random rnd = new Random();
+            Thread.Sleep(50);
+            tryFix = Convert.ToBoolean(rnd.Next(0, 1));
+
+            if (tryFix)
+            {
+                Difficulty--;
+                if (Difficulty <= 0)
+                {
+                    Console.WriteLine($"{Name} is fixed!");
+                    return IsFixed = true;
+                }
+                else
+                {
+                    return IsFixed;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Program can't solve {Name} at this time");
+                return IsFixed;
+            }
+        }
+    }
+}
---
+++ STATIC/STATIC/Static.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassTask;
+
+namespace STATIC
+{
+    public static class Static
+    {   
+
+        /// <summary>
+        /// Count summary difficulty of all tasks and 
+        /// tells is it possible to solve this ammount of task
+        /// in 30 itterations
+        /// </summary>
+        /// <param name="tasks"></param>
+        /// <returns>Boolean meaning depends of solution </returns>
+        public static bool CheckDif(List<ClassTask.Task> tasks)
+        {
+            bool canDecide;
+            double counter = 0;
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                if (tasks[i].Difficulty >0)
+                {
+                    counter += tasks[i].Difficulty;
+                }
+            }
+            if (counter > 30)
+            {
+                Console.WriteLine("Sorry, there is too much tasks. Program couldn't solve it");
+                return canDecide = false;
+            }
+            else
+            {
+                return canDecide = true;
+            }
+        }
+        public static bool IsValueDouble(string input)
+        {
+            if (double.TryParse(input, result))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
